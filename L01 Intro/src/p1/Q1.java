ee8c5b4a24084395c87def305dd9cbb996f47d8a
B. Chen <chenb@sunysuffolk.edu>
2026-02-18 11:13:50 -0500

    lecture 1

---
+++ L01 Intro/src/p1/Q1.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class Q1 {
+	public static void main(String[] args) {
+		int x = 7;
+		int y = 5;
+		System.out.println("Sum: " + (x + y));
+		System.out.println("Quotient: " + ((double) x / y));
+		System.out.println("Remainder: " + (x % y));
+	}
+
+}
---
+++ L01 Intro/src/p1/Q2.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Q2 {
+	public static void main(String[] arg) {
+		String greeting = "Hello World!";
+		System.out.println(greeting.length());
+		System.out.println(greeting.toUpperCase());
+		System.out.println(greeting.contains("or"));
+		System.out.println(greeting.indexOf("pr"));
+	}
+}
